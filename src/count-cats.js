72757a0e822df37760c1aba050c6e0d123db9465
Dzimitri <d.prankevich@gmail.com>
2020-03-22 23:18:05 +0300

    feat: add decision

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  var sum = 0;
+  var mCol = [];
+  for (var i = 0; i < matrix.length; i++){
+    mCol = matrix[i];
+    for ( var j = 0; j < mCol.length; j++){
+      if( mCol[j] != "^^") continue;
+      sum++;
+  }
+} return sum;
+
 };
